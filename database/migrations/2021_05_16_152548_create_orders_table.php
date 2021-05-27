b71830b2a13e224c5713b1141b7ee962028c5f02
SirPeace <roman.khabibulin12@gmail.com>
2021-05-27 22:49:00 +0300

    Fix orders migration foreign keys

--- database/migrations/2021_05_16_152548_create_orders_table.php
+++ database/migrations/2021_05_16_152548_create_orders_table.php
@@ -16,8 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->foreignId('currencies');
-            $table->foreignId('order_statuses');
+            $table->foreignId('order_status_id');
             $table->text('user_note');
             $table->timestamps();
         });
