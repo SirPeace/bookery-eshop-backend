8473bdae1714c91e063fcc73c444ab7d8fb22155
SirPeace <roman.khabibulin12@gmail.com>
2021-05-28 14:52:24 +0300

    Make Product model sluggable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,13 +2,23 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 
     public function category(): BelongsTo
     {
