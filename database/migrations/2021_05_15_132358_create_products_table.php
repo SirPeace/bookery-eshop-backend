9d70d7a2ba4c765e55075328d17a07a9b38338e5
SirPeace <roman.khabibulin12@gmail.com>
2021-05-28 14:56:16 +0300

    Update products migration

--- database/migrations/2021_05_15_132358_create_products_table.php
+++ database/migrations/2021_05_15_132358_create_products_table.php
@@ -15,17 +15,19 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_category_id');
+            $table->foreignId('category_id');
             $table->unsignedDecimal('price');
             $table->unsignedSmallInteger('discount')->default(0);
             $table->string('title');
             $table->string('slug');
-            $table->string('thumbnail_url');
+            $table->string('thumbnail_path')->default(
+                'public/product-images/default.png'
+            );
             $table->text('description');
             $table->text('keywords');
             $table->boolean('active')->default(true);
-            $table->timestamp('active_since');
-            $table->timestamp('archived_since');
+            $table->timestamp('active_since')->nullable();
+            $table->timestamp('archived_since')->nullable();
             $table->timestamps();
         });
     }
