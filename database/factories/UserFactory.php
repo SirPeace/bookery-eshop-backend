567b2699e1be850ddb6ba19e407ce2b6aeadb3d8
SirPeace <roman.khabibulin12@gmail.com>
2021-05-28 14:54:05 +0300

    Update User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,16 +23,11 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $customerRoleId = Role::factory()->create([
-            'name' => 'customer',
-            'alias' => 'customer'
-        ])->id;
-
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'role_id' => $customerRoleId,
+            'role_id' => Role::factory(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
