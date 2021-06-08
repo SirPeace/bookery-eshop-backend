b327760328a196306ea0761f55924f8cefd07731
SirPeace <roman.khabibulin12@gmail.com>
2021-06-08 18:08:08 +0300

    Test orders relationship

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Unit\Models;
 
+use Tests\TestCase;
 use App\Models\Role;
 use App\Models\User;
+use App\Models\Order;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
 class UserTest extends TestCase
 {
@@ -17,12 +18,22 @@ class UserTest extends TestCase
     {
         parent::setUp();
 
-        $this->user = User::factory()->for(Role::factory())->create();
+        $this->user = User::factory()
+            ->for(Role::factory())
+            ->has(Order::factory()->count(3))
+            ->create();
     }
 
     /** @test */
     public function user_belongs_to_role()
     {
         $this->assertInstanceOf(Role::class, $this->user->role);
+    }
+
+    /** @test */
+    public function user_has_many_orders()
+    {
+        $this->assertContainsOnlyInstancesOf(Order::class, $this->user->orders);
+        $this->assertCount(3, $this->user->orders);
     }
 }
