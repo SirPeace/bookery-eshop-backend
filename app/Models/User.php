e1c2e2470db86b74d378b417564ef45a5eeea067
SirPeace <roman.khabibulin12@gmail.com>
2021-06-11 19:44:35 +0300

    Delete unnecessary imports

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use App\Traits\HasAvatar;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
