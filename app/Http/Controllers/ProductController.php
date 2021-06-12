a308f7c4e5e30f2aa22dbcf47ae9c9bdc9dd3a6c
SirPeace <roman.khabibulin12@gmail.com>
2021-06-12 20:43:13 +0300

    Filter products logic

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,26 +3,116 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\ProductStoreRequest;
 use App\Http\Requests\ProductUpdateRequest;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::all();
+        $paginationCount = $request['count'] ?? 20;
+        $user = User::query()->find(auth()->id());
+
+        $products = Product::query()
+            ->select(
+                'products.id',
+                'products.category_id',
+                'products.old_price',
+                'products.price',
+                'products.discount',
+                'products.title',
+                'products.slug',
+                'products.thumbnail_path',
+                'products.description',
+                'products.keywords'
+            )
+            ->when(
+                $request['category'],
+                fn (Builder $query) => $query->where(
+                    'category_id',
+                    $request['category']
+                )
+            )
+            ->when(
+                $request['min_price'],
+                fn (Builder $query) => $query->where(
+                    'price',
+                    '>=',
+                    $request['min_price']
+                )
+            )
+            ->when(
+                $request['max_price'],
+                fn (Builder $query) => $query->where(
+                    'price',
+                    '<=',
+                    $request['max_price']
+                )
+            )
+            ->when(
+                $request['with_thumbnail'],
+                fn (Builder $query) => $query->where(
+                    'thumbnail_path',
+                    '<>',
+                    'public/product-thumbnails/default.png'
+                )
+            )
+            ->when(
+                $request['attribute'] && $request['attribute_value'],
+                fn (Builder $query) => $query
+                    ->join(
+                        'categories',
+                        'products.category_id',
+                        '=',
+                        'categories.id'
+                    )
+                    ->join(
+                        'attribute_group_category',
+                        'categories.id',
+                        '=',
+                        'attribute_group_category.category_id'
+                    )
+                    ->join(
+                        'attribute_product',
+                        'products.id',
+                        '=',
+                        'attribute_product.product_id'
+                    )
+                    ->join(
+                        'attributes',
+                        'attributes.id',
+                        '=',
+                        'attribute_product.attribute_id'
+                    )
+                    ->join(
+                        'attribute_groups',
+                        'attribute_groups.id',
+                        '=',
+                        'attribute_group_category.attribute_group_id'
+                    )
+                    ->where('attribute_groups.slug', $request['attribute'])
+                    ->where('attributes.value', $request['attribute_value'])
+            )
+            ->when(
+                !in_array($user?->role->slug, ['admin', 'manager']),
+                fn (Builder $query) => $query->where('active', true)
+            )
+            ->paginate($paginationCount);
 
         return response()->json([
-            'status' => 'success',
-            'data'   => [
-                'products' => $products
+            "status" => "success",
+            "data" => [
+                "products" => $products,
             ]
         ]);
     }
--- tests/Feature/Product/GetProductsTest.php
+++ tests/Feature/Product/GetProductsTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature\Product;
 
+use App\Models\Attribute;
+use App\Models\AttributeGroup;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -11,17 +14,78 @@ class GetProductsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function get_all_products()
+    public function get_paginated_products()
     {
-        [$productOne, $productTwo] = Product::factory(2)->create();
+        [0 => $firstProduct, 99 => $lastProduct] = Product::factory(100)->create();
+
+        $firstProduct->refresh();
+        $lastProduct->refresh();
 
         $this->get(route('products.index'))
             ->assertJson([
                 'status' => 'success',
                 'data'   => [
-                    'products' => [
-                        ['title' => $productOne->title],
-                        ['title' => $productTwo->title]
+                    "products" => [
+                        "current_page" => 1,
+                        "data" => [], // assertJsonFragment() here
+                        "first_page_url" => "http://localhost/api/products?page=1",
+                        "from"           => 1,
+                        "last_page"      => 5,
+                        "last_page_url"  => "http://localhost/api/products?page=5",
+                        "links" => [
+                            [
+                                "url"    => null,
+                                "label"  => "&laquo; Previous",
+                                "active" => false
+                            ],
+                            [
+                                "url"    => "http://localhost/api/products?page=1",
+                                "label"  => "1",
+                                "active" => true
+                            ],
+                            [
+                                "url"    => "http://localhost/api/products?page=2",
+                                "label"  => "2",
+                                "active" => false
+                            ],
+                        ],
+                        "next_page_url" => "http://localhost/api/products?page=2",
+                        "path"          => "http://localhost/api/products",
+                        "per_page"      => 20,
+                        "prev_page_url" => null,
+                        "to"            => 20,
+                        "total"         => 100
+                    ]
+                ]
+            ])
+            ->assertJsonFragment([
+                "id"             => $firstProduct->id,
+                "category_id"    => $firstProduct->category_id,
+                "old_price"      => (string) $firstProduct->old_price,
+                "price"          => (string) $firstProduct->price,
+                "discount"       => $firstProduct->discount,
+                "title"          => $firstProduct->title,
+                "slug"           => $firstProduct->slug,
+                "thumbnail_path" => $firstProduct->thumbnail_path,
+                "description"    => $firstProduct->description,
+                "keywords"       => $firstProduct->keywords,
+            ])
+            ->assertJsonMissing([
+                "id"   => $lastProduct->id,
+                "slug" => $lastProduct->slug,
+            ]);
+
+        $this->get(route('products.index', ['page' => 5]))
+            ->assertJson([
+                "data" => [
+                    "products" => [
+                        "current_page" => 5,
+                        "data" => [
+                            19 => [
+                                "id"   => $lastProduct->id,
+                                "slug" => $lastProduct->slug,
+                            ]
+                        ]
                     ]
                 ]
             ]);
@@ -38,6 +102,61 @@ class GetProductsTest extends TestCase
                 'data'   => [
                     'product' => [
                         'title' => 'Product one'
+                    ]
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function can_filter_products_by_category()
+    {
+        Product::factory(2)->create();
+        $category = Category::factory()->create();
+        $product = Product::factory()->create(['category_id' => $category->id]);
+
+        $this->get(route('products.index', ['category' => $category->id]))
+            ->assertJsonCount(1, 'data.products.data')
+            ->assertJson([
+                'data' => [
+                    'products' => [
+                        'data' => [
+                            [
+                                'id'          => $product->id,
+                                'category_id' => $category->id
+                            ]
+                        ]
+                    ]
+                ]
+            ]);
+    }
+
+    /** @test */
+    public function can_filter_products_by_single_attribute()
+    {
+        Product::factory(2)->create();
+
+        $attrbuteGroup = AttributeGroup::factory()->create(['slug' => 'author']);
+        $category = Category::factory()->create();
+        $category->attribute_groups()->save($attrbuteGroup);
+
+        $product = Product::factory()->create(['category_id' => $category->id]);
+        $attribute = Attribute::factory()->create(['value' => 'Oscar Hartmann']);
+        $product->attributes()->save($attribute);
+
+        $this->get(route(
+            'products.index',
+            [
+                'attribute' => 'author',
+                'attribute_value' => 'Oscar Hartmann'
+            ]
+        ))
+            ->assertJsonCount(1, 'data.products.data')
+            ->assertJson([
+                'data' => [
+                    'products' => [
+                        'data' => [
+                            ['id' => $product->id]
+                        ]
                     ]
                 ]
             ]);
